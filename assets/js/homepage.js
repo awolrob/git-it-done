b16d2962337012dd73bbb2943fda34f8f6cbaf24
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-06-23 22:33:15 -0400

    add catch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -21,18 +21,31 @@ var getUserRepos = function (user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a get request to url
-  fetch(apiUrl).then(function (response) {
-    console.log(response);
-    response.json().then(function (data) {
-      displayRepos(data, user);
-    });
+  fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data, user);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
   });
 };
 
 getUserRepos("awolrob");
 
 var displayRepos = function (repos, searchTerm) {
-  // clear old content
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }  // clear old content
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
   console.log(repos);
