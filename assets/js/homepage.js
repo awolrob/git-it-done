c7b645cdb87be37d984235a7a9e506d7e26c1215
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-06-23 22:59:53 -0400

    6.3.7 Reflection

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,53 +3,56 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
-var formSubmitHandler = function (event) {
+var formSubmitHandler = function(event) {
+  // prevent page from refreshing
   event.preventDefault();
+
   // get value from input element
   var username = nameInputEl.value.trim();
 
   if (username) {
     getUserRepos(username);
+
+    // clear old content
+    repoContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
     alert("Please enter a GitHub username");
   }
 };
 
-var getUserRepos = function (user) {
+var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a get request to url
   fetch(apiUrl)
-  .then(function(response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function(data) {
-        displayRepos(data, user);
-      });
-    } else {
-      alert('Error: GitHub User Not Found');
-    }
-  })
-  .catch(function(error) {
-    // Notice this `.catch()` getting chained onto the end of the `.then()` method
-    alert("Unable to connect to GitHub");
-  });
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      alert("Unable to connect to GitHub");
+    });
 };
 
-getUserRepos("awolrob");
-
-var displayRepos = function (repos, searchTerm) {
+var displayRepos = function(repos, searchTerm) {
   // check if api returned any repos
   if (repos.length === 0) {
     repoContainerEl.textContent = "No repositories found.";
     return;
-  }  // clear old content
-  repoContainerEl.textContent = "";
+  }
+
   repoSearchTerm.textContent = searchTerm;
-  console.log(repos);
-  console.log(searchTerm);
+
   // loop over repos
   for (var i = 0; i < repos.length; i++) {
     // format repo name
@@ -65,6 +68,7 @@ var displayRepos = function (repos, searchTerm) {
 
     // append to container
     repoEl.appendChild(titleEl);
+
     // create a status element
     var statusEl = document.createElement("span");
     statusEl.classList = "flex-row align-center";
@@ -79,10 +83,11 @@ var displayRepos = function (repos, searchTerm) {
 
     // append to container
     repoEl.appendChild(statusEl);
+
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
   }
 };
 
-
+// add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
